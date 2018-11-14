f1c7212ef572cc37fc0240b71b691a319fffbb7b
mikexie <mxie622@gmail.com>
2018-11-15 02:09:16 +1300

    # First leetcode exercise page

--- easy_Question_solution.py
+++ easy_Question_solution.py
@@ -220,3 +220,12 @@ class Solution:
                 return True
             else:
                 return False
+
+# https://leetcode.com/problems/contains-duplicate/description/
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
